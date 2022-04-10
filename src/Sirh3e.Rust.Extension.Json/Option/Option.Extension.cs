8bf0582b2737aa512a7b1dde8ee8576be816b1bf
sirh3e <marvin.bluewin@bluewin.ch>
2022-04-10 16:03:45 +0200

    Added support for option json

---
+++ src/Sirh3e.Rust.Extension.Json/Option/Option.Extension.cs
@@ -0,0 +1,12 @@
+﻿namespace Sirh3e.Rust.Option;
+
+public static class Extension
+{
+    public static string ToJson<TSome>(this Option<TSome>       option) => ToJson((OptionAsJson<TSome>)option);
+    public static string ToJson<TSome>(this OptionAsJson<TSome> json)   => JsonSerializer.Serialize(json);
+
+    public static OptionAsJson<TSome> ToJsonObject<TSome>(this Option<TSome>       option)  => option;
+    public static Option<TSome>       ToOption<TSome>(this     OptionAsJson<TSome> @object) => @object;
+
+    public static Option<TSome> FromJson<TSome>(string text) => JsonSerializer.Deserialize<OptionAsJson<TSome>>(text) ?? Option<TSome>.None;
+}
---
+++ src/Sirh3e.Rust.Extension.Json/Usings.cs
@@ -0,0 +1,4 @@
+﻿global using System.Text.Json;
+global using System.Text.Json.Serialization;
+global using Sirh3e.Rust.Option;
+global using Sirh3e.Rust.Extension.Json.Option;
